0feb0027738446a558d579e7fc846dd4076daf2b
KavinMadusanka <kavinmadusanka20011@gmail.com>
2024-10-02 15:26:04 +0530

    back end

---
+++ Backend/server.js
@@ -0,0 +1,39 @@
+import express from "express";
+import colors from "colors";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import connectDB from "./config/db.js";
+
+import cors from "cors";
+
+//configure env
+dotenv.config();
+
+//database config
+connectDB();
+
+//rest object
+const app = express();
+
+//middelwares
+app.use(cors())
+app.use(express.json())
+app.use(morgan('dev'))
+
+//Route
+
+
+// rest api
+app.get("/" , (req,res) => {
+    res.send({
+        message: "Welcome to  Smart Waste Management System",
+    });
+});
+
+//Port
+const PORT = process.env.PORT || 8082;
+
+//run listen
+app.listen(PORT, () => {
+    console.log(`Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan.white);
+});
