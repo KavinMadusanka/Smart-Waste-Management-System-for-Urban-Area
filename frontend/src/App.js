ada3bca1d1740e595de4cca281d939cf1e209676
Sandamini Indrachapa <130564612+SandaminiI@users.noreply.github.com>
2024-10-04 17:06:27 +0530

    Created frontend login part
    Co-Authored-By: Kavin Madhusankha <163830061+KavinMadusanka@users.noreply.github.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route} from "react-router-dom"
 import Register from "./Pages/Auth/Register";
 import HomePage from "./Pages/HomePage";
 import PageNotFound from "./Pages/PageNotFound";
+import Login from "./Pages/Auth/Login";
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Route path="/" element={<HomePage />} />
       <Route path="*" element={<PageNotFound />} />
       <Route path='/register' element={<Register/>}/>
+      <Route path='login' element={<Login />} />
     </Routes>
     
      
