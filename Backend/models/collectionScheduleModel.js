aa390fd4d8287e78a87602be80714d95ae57a2fb
Sandamini Indrachapa <130564612+SandaminiI@users.noreply.github.com>
2024-10-04 22:50:15 +0530

    Create collection schedule model
    Co-Authored-By: Kavin Madhusankha <163830061+KavinMadusanka@users.noreply.github.com>

---
+++ Backend/models/collectionScheduleModel.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+// Create Collection Schedule Schema
+const collectionScheduleSchema = new mongoose.Schema({
+    area: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    pickupDate: {
+        type: Date,
+        required: true
+    },
+    pickupTime: {
+        type: String,
+        required: true
+    },
+    binType: {
+        type: String,
+        enum: ['General', 'Recyclable', 'Bulk'],
+        required: true
+    },
+    assignedCollector: {
+        type: mongoose.ObjectId,
+        ref: 'users', // Reference to WasteCollector from the User model
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ['Scheduled', 'In Progress', 'Completed', 'Missed'],
+        default: 'Scheduled'
+    },
+});
+
+export default mongoose.model('CollectionSchedule', collectionScheduleSchema);
