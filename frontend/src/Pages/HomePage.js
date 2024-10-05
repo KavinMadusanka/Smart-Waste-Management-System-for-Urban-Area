f9a5080fc252721a5b9cc9ce75ddc9ac57005507
Navishka Gunawardana <128272519+it22570444@users.noreply.github.com>
2024-10-05 19:29:48 +0530

    Add Header

--- frontend/src/Pages/HomePage.js
+++ frontend/src/Pages/HomePage.js
@@ -1,8 +1,14 @@
-import React from 'react'
+import React from 'react';
+import { Typography, Button, Container, Box, Grid, Paper } from '@mui/material';
+import { Link } from 'react-router-dom';
+import Layout from '../components/Layout/Layout';
+//import mall from './../assets/mall.webp'; // Correct import for the background image
 
 const HomePage = () => {
   return (
+    <Layout>
     <div>HomePage</div>
+    </Layout>
   )
 }
 
