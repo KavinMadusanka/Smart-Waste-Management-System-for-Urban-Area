a03c3cbb1040ebb823590547445874b0ba931ece
Sandamini Indrachapa <130564612+SandaminiI@users.noreply.github.com>
2024-10-12 20:21:03 +0530

    Update AuthRoute.js

--- Backend/routes/AuthRoute.js
+++ Backend/routes/AuthRoute.js
@@ -1,18 +1,14 @@
 import express from 'express'
 import { isAdmin, requireSignIn } from '../middlewares/AuthMiddleware.js'
-import { getAllWasteCollectors, loginController, registerController } from '../controllers/AuthController.js'
+import { getAllWasteCollectors, LoginController, userRegisterController, wasteCollectorRegisterController } from '../controllers/AuthController.js'
 
 //router object
 const router = express.Router()
 
 //routing path
-//Register || post method
-router.post('/register',registerController )
-
-//login
-router.post('/login',loginController)
-
-//get all waste collectors
-router.get('/get-all-collectors',getAllWasteCollectors)
+router.get('/get-all-collectors',getAllWasteCollectors )
+router.post('/registerUser',userRegisterController)
+router.post('/registerCollector',wasteCollectorRegisterController)
+router.post('/login',LoginController)
 
 export default router
