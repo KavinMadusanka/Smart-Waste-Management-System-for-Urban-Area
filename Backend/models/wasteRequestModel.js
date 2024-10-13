1d44229e54aee920567f89b30895c8ac19e631e8
KavinMadusanka <kavinmadusanka20011@gmail.com>
2024-10-14 05:06:37 +0530

    Create wasteRequestModel.js
    Co-Authored-By: Piyusha Prabhasari <159011784+Prabhasari@users.noreply.github.com>
    Co-Authored-By: Navishka Gunawardana <128272519+navishkaG@users.noreply.github.com>
    Co-Authored-By: Sandamini Indrachapa <130564612+SandaminiI@users.noreply.github.com>

---
+++ Backend/models/wasteRequestModel.js
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose';
+
+// Define the waste request schema
+const wasteRequestSchema = new mongoose.Schema({
+  items: [
+    {
+        category: {
+            type: String,
+            required: true,
+        },
+        quantity: {
+            type: Number,
+            required: true,
+        },
+        points: { 
+            type: Number, 
+            required: true 
+        },
+    },
+  ],
+  userEmail: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    default: 'pending',
+  },
+}, { timestamps: true });
+
+export default mongoose.model('WasteRequest', wasteRequestSchema);
