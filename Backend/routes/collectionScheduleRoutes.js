4054f092ed243922e2e4137536139d619601c3c4
Sandamini Indrachapa <130564612+SandaminiI@users.noreply.github.com>
2024-10-13 15:44:14 +0530

    Update collectionScheduleRoutes.js

--- Backend/routes/collectionScheduleRoutes.js
+++ Backend/routes/collectionScheduleRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { requireSignIn } from '../middlewares/AuthMiddleware.js'
-import { createCollectionSchedule, getAllCollectionSchedules, getCollectionScheduleById,  getScheduleByWasteCollectorController,  updateCollectionSchedule } from './../controllers/collectionScheduleController.js';
+import { createCollectionSchedule, getAllCollectionSchedules, getCollectionScheduleById,  getScheduleByWasteCollectorController,  updateCollectionSchedule, updateScheduleStatus } from './../controllers/collectionScheduleController.js';
 
 //router object
 const router = express.Router()
@@ -20,5 +20,8 @@ router.get('/get-schedules/:id', getCollectionScheduleById );
 
 //get schedule by collector
 router.get('/get-schedule-by-collector/:assignedCollectorId',getScheduleByWasteCollectorController )
+
+// Define the PUT route for updating schedule status
+router.put('/update-status/:id', updateScheduleStatus );
 
 export default router
